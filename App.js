34821bb70adb323e50544c32aaa703b7a35e6983
Héctor Sabillón <h.hsabillon@gmail.com>
2020-11-30 14:59:18 -0600

    Verifica que se crea las tablas en la pantalla de spalsh

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import useDatabase from "./src/hooks/useDatabase";
+import { NotesContextProvider } from "./src/context/NotesContext";
 
 export default function App() {
+  // Prevenir que la pantalla de splash se oculte
+  SplashScreen.preventAutoHideAsync();
+
+  const isLoadingComplete = useDatabase();
+
+  // Ocutar la pantalla de splash
+  if (isLoadingComplete) SplashScreen.hideAsync();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NotesContextProvider>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </NotesContextProvider>
     </View>
   );
 }
@@ -14,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
