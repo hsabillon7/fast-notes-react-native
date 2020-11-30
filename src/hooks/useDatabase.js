32257f5aafefeb8e596756ecafb002613e1436e6
Héctor Sabillón <h.hsabillon@gmail.com>
2020-11-30 14:58:07 -0600

    hook que permite utilizar los métodos de la DB

---
+++ src/hooks/useDatabase.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { database } from "../components/db";
+
+const useDatabase = () => {
+  const [isLoadingComplete, setIsLoadingComplete] = useState(false);
+
+  const loadDatabase = async () => {
+    try {
+      // await database.dropDatabaseTableAsync();
+      await database.setupDatabaseTableAsync();
+
+      // Finaliza la carga de la DB
+      setIsLoadingComplete(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadDatabase();
+  }, []);
+
+  return isLoadingComplete;
+};
+
+export default useDatabase;
