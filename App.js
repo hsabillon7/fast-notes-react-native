35c0fd7a7adf3f39d73f4e50c9393e0da4a5de9b
Héctor Sabillón <h.hsabillon@gmail.com>
2020-12-03 14:55:38 -0600

    Implementación del provider y de las pantallas de navegación vía Stack navigator

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import useDatabase from "./src/hooks/useDatabase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { NotesContextProvider } from "./src/context/NotesContext";
+import NotesListScreen from "./src/screens/NotesListScreen";
+import NoteCreateScreen from "./src/screens/NoteCreateScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   // Prevenir que la pantalla de splash se oculte
@@ -11,24 +16,19 @@ export default function App() {
 
   const isLoadingComplete = useDatabase();
 
-  // Ocutar la pantalla de splash
+  // Ocultar la pantalla de splash
   if (isLoadingComplete) SplashScreen.hideAsync();
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       <NotesContextProvider>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="notesList">
+            <Stack.Screen name="notesList" component={NotesListScreen} />
+            <Stack.Screen name="noteCreate" component={NoteCreateScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </NotesContextProvider>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
